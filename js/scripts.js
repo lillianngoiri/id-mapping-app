b3315b4c3c130c3dd526f245ed051419d2597a56
omoshthegeek <omoshthegeek@gmail.com>
2019-11-14 12:50:32 +0300

    fix script function

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,49 @@
-// function for displaing image
+// function for displaing image 
 function readURL(input) {
-           if (input.files && input.files[0]) {
-               var reader = new FileReader();
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
 
-               reader.onload = function (e) {
-                   $('#blah')
-                       .attr('src', e.target.result);
-               };
+        reader.onload = function (e) {
+            $('#blah')
+                .attr('src', e.target.result);
+        };
 
-               reader.readAsDataURL(input.files[0]);
-           }
-       }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
 //Bussiness Logic
 
-    //function for capturing user data and generating the student id
+//function for capturing user data and generating the student id
 $(document).ready(
-    function(){
-        $("#create").click(
-            function(){
-                var fName = $("#first-name").val();
-                var lName= $("#second-name").val();
-                var admNo= $("#adm-no").val();
-                var idNo = $("#id-no").val();
-                var course = $("#course").val();
-                var gender = $("#gender input:checked").val();
-                // alert(fName);
-                // alert(lName)
-                // alert(admNo)
-                // alert(idNo)
-                // alert(course)
-                // alert(gender)
-
-                  //output details
-                $("#names").text(fName+"       "+lName);
-                $("#adm-no").text(admNo);
-                $("#id-no").text(idNo);
-                $("#course").text(course);
-                $("#gender").text(gender);
+function(){
+ $("#create").click(
+     function(e){
+         var fName = $("#first-name").val();
+         var lName= $("#second-name").val();
+         var admNo= $("#adm-no").val();
+         var idNo = $("#id-no").val();
+         var course = $("#course").val();
+         var gender = $("#gender input:checked").val();
+         // alert(fName);
+         // alert(lName)
+         // alert(admNo)
+         // alert(idNo)
+         // alert(course)
+         // alert(gender)
 
 
+         $("#names").text(fName+"       "+lName);
+         $("#admno").text(admNo);
+         $("#idno").text(idNo);
+         $("#course1").text(course);
+         $("#gender1").text(gender);
 
+        
+         e.preventDefault();
 
-            }
-        )
-        preventdefault();
-    }
+     }
+ )
+ 
+}
 
 )
